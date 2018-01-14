6558b06a3c09f7b33619a68908ba9635328fdd05
Cliff Rodgers <kliph@users.noreply.github.com>
2018-01-14 18:51:02 -0500

    Add salutations and outros
    - Refactor spec to move options/arguments to outermost scope

--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -5,39 +5,26 @@ ENV['ENV'] = 'test'
 
 load './question'
 
-RSpec.describe '#salutation' do
-  context 'with a name option' do
-    it 'returns a salutation' do
-      expect(salutation('Test')).to include 'Test'
-    end
+RSpec.context 'with options' do
+  let(:name) { 'Bob' }
+  let(:question) { 'some question text' }
+  let(:options) { ['-n', name, '--'] << question.split(' ') }
+  let(:parsed_options) { { name: name } }
+  let!(:warn_level) { $VERBOSE }
+  let!(:orig_ARGV) { ARGV }
+
+  # Suppress warnings about reassigning ARGV
+  before(:each) do
+    $VERBOSE = nil
+    ARGV = options
   end
-end
 
-RSpec.describe '#parse_options!' do
-  context 'without options' do
-    it 'sets :name to nil' do
-      expect(parse_options![:name]).to be_nil
-    end
+  after(:each) do
+    ARGV = orig_ARGV
+    $VERBOSE = warn_level
   end
 
-  context 'with options' do
-    let(:name) { 'Bob' }
-    let(:question) { 'some question text' }
-    let(:options) { ['-n', name, '--'] << question.split(' ') }
-    let!(:warn_level) { $VERBOSE }
-    let!(:orig_ARGV) { ARGV }
-
-    # Suppress warnings about reassigning ARGV
-    before(:each) do
-      $VERBOSE = nil
-      ARGV = options
-    end
-
-    after(:each) do
-      ARGV = orig_ARGV
-      $VERBOSE = warn_level
-    end
-
+  describe '#parse_options!' do
     it 'sets the :name' do
       expect(parse_options![:name]).to eq name
     end
@@ -45,6 +32,49 @@ RSpec.describe '#parse_options!' do
     it 'sets the question text to the argument following --' do
       parse_options!
       expect(question_text).to eq question
+    end
+  end
+
+  describe '#salutation' do
+    context 'with a name option' do
+      it 'returns a salutation' do
+        expect(salutation('Test')).to include 'Test'
+      end
+    end
+  end
+
+  describe '#format_output' do
+    it 'includes the name' do
+      expect(format_output(parsed_options)).to include name
+    end
+
+    it 'ends with a period' do
+      expect(format_output(parsed_options)).to match(/.\z/)
+    end
+
+    it 'includes the question text' do
+      expect(format_output(parsed_options)).to include question
+    end
+  end
+end
+
+RSpec.context 'without options' do
+  let(:options) { {} }
+  describe '#parse_options!' do
+    it 'sets :name to nil' do
+      expect(parse_options![:name]).to be_nil
+    end
+  end
+
+  describe '#format_output' do
+    it 'ends with a period' do
+      expect(format_output(options)).to match(/.\z/)
+    end
+  end
+
+  describe '#outro' do
+    it 'does emdashes correctly' do
+      expect(outro).not_to match(/-- /)
     end
   end
 end
